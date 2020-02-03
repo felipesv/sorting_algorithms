afcab169b3307fd0f705ddf05552c2c4bfa2122e
Felipe Satizabal <1165@holbertonschool.com>
2020-02-03 16:27:01 -0500

    fix 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *crn;
 
-	if (list == NULL || *list == NULL || crn->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	crn = *list;
